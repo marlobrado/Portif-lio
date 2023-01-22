a2f74e20e145d7a8abe4e1429ed292abfe8f04dd
Fernando <fernandogynbsb@gmail.com>
2023-01-22 14:57:05 -0300

    Finalizando

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -16,98 +16,109 @@ import { FaGithub } from "react-icons/fa";
 
 // Page Style
 import {
-  Header,
-  HeaderContent,
-  HeaderButtonsArea,
-  UserImage,
-  StackCards,
-  ProjectsArea,
-  ProjectsAreaSocialMediaMessage,
-  ProjectAreaWrapperColumns,
-  ProjectsAreaContent,
+    Header,
+    HeaderContent,
+    HeaderButtonsArea,
+    UserImage,
+    StackCards,
+    ProjectsArea,
+    ProjectsAreaSocialMediaMessage,
+    ProjectAreaWrapperColumns,
+    ProjectsAreaContent,
 } from "./style";
 
 export const Home = (): JSX.Element => {
-  const gihubUrl = `https://github.com/${userData.githubUser}`;
-  const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
+    const gihubUrl = `https://github.com/${userData.githubUser}`;
+    const portfolioUrl = `https://github.com/${userData.githubUser}/  Portif-lio`;
 
-  return (
-    <main id="home">
-      <Header>
-        <Container>
-          <HeaderContent>
-            <Flex>
-              <UserImage
-                src={`https://github.com/${userData.githubUser}.png`}
-                alt={userData.nameUser}
-                title={userData.nameUser}
-                width={"48px"}
-                height={"48px"}
-              />
-              <Text color="grey4">Hello, my name is {userData.nameUser}</Text>
-            </Flex>
-            <Text as="h1" type="heading1" color="grey5">
-            I{" "}
-              <Text as="span" type="heading1" color="brand1">
-                love
-              </Text>{" "}
-              creating and{" "}
-              <Text as="span" type="heading1" color="brand1">
-                developing
-              </Text>{" "}
-              projects
-            </Text>
-            <Text type="body1" color="grey2">
-              Discover here in this environment, created especially for you, all
-              my projects and technologies
-            </Text>
-            <HeaderButtonsArea>
-              <Button as="a" type="primary" href="#projects">
-                See Projects
-              </Button>
-              <Button as="a" type="outline" target="_blank" href={portfolioUrl}>
-                See my portfolio source code
-              </Button>
-              <Button
-                color="grey5"
-                as="a"
-                css={{ "&:hover": { color: "$grey1" } }}
-                type="circle"
-                target="_blank"
-                href={gihubUrl}
-              >
-                <FaGithub />
-              </Button>
-            </HeaderButtonsArea>
-            <StackCards>
-              {stackData.map((stack, index) => (
-                <Stack key={index} title={stack.title} icon={stack.img} />
-              ))}
-            </StackCards>
-          </HeaderContent>
-        </Container>
-      </Header>
-      <ProjectsArea id="projects">
-        <Container>
-          <ProjectAreaWrapperColumns>
-            <ProjectsAreaSocialMediaMessage>
-              <Text as="h2" type="heading4" color="grey4">
-                My projects
-              </Text>
-              <Text as="p" type="body1" color="grey2">
-                Some of my{" "}
-                <Text as="span" color="brand5">
-                  side projects
-                </Text>
-              </Text>
-            </ProjectsAreaSocialMediaMessage>
-            <ProjectsAreaContent>
-              <Project />
-            </ProjectsAreaContent>
-          </ProjectAreaWrapperColumns>
-        </Container>
-      </ProjectsArea>
-      <Contacts />
-    </main>
-  );
+    return (
+        <main id="home">
+            <Header>
+                <Container>
+                    <HeaderContent>
+                        <Flex>
+                            <UserImage
+                                src={`https://github.com/${userData.githubUser}.png`}
+                                alt={userData.nameUser}
+                                title={userData.nameUser}
+                                width={"48px"}
+                                height={"48px"}
+                            />
+                            <Text color="grey4">
+                                Olá, meu nome é {userData.nameUser} e esta é o
+                                meu portifólio
+                            </Text>
+                        </Flex>
+                        <Text as="h1" type="heading1" color="grey5">
+                            Eu sou{" "}
+                            <Text as="span" type="heading1" color="brand1">
+                                desenvolvedor WEB
+                            </Text>{" "}
+                            e amo trabalhar com{" "}
+                            <Text as="span" type="heading1" color="brand1">
+                                tecnologia.
+                            </Text>{" "}
+                        </Text>
+                        <Text type="body1" color="grey2">
+                            Descubra aqui neste ambiente, criado especialmente
+                            para você, todos meus projetos e tecnologias
+                        </Text>
+                        <HeaderButtonsArea>
+                            <Button as="a" type="primary" href="#projects">
+                                Ver Projetos
+                            </Button>
+                            <Button
+                                as="a"
+                                type="outline"
+                                target="_blank"
+                                href={portfolioUrl}
+                            >
+                                Veja o código-fonte do meu portfólio
+                            </Button>
+                            <Button
+                                color="grey5"
+                                as="a"
+                                css={{ "&:hover": { color: "$grey1" } }}
+                                type="circle"
+                                target="_blank"
+                                href={gihubUrl}
+                            >
+                                <FaGithub />
+                            </Button>
+                        </HeaderButtonsArea>
+                        <StackCards>
+                            {stackData.map((stack, index) => (
+                                <Stack
+                                    key={index}
+                                    title={stack.title}
+                                    icon={stack.img}
+                                />
+                            ))}
+                        </StackCards>
+                    </HeaderContent>
+                </Container>
+            </Header>
+            <ProjectsArea id="projects">
+                <Container>
+                    <ProjectAreaWrapperColumns>
+                        <ProjectsAreaSocialMediaMessage>
+                            <Text as="h2" type="heading4" color="grey4">
+                                Meus Projetos
+                            </Text>
+                            <Text as="p" type="body1" color="grey2">
+                                Alguns dos meus{" "}
+                                <Text as="span" color="brand5">
+                                    projetos paralelos
+                                </Text>
+                            </Text>
+                        </ProjectsAreaSocialMediaMessage>
+                        <ProjectsAreaContent>
+                            <Project />
+                        </ProjectsAreaContent>
+                    </ProjectAreaWrapperColumns>
+                </Container>
+            </ProjectsArea>
+            <Contacts />
+        </main>
+    );
 };
--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
+  nameUser: "Fernando Henrique",
+  githubUser: "marlobrado",
   linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  whatsappNumber: "+5562981540735",
+  emailUser: "fernandogynbsb@gmail.com  ",
+  instagramUser: "fernandohstt",
   facebookUser: "",
 };
